499f616412153955cde02f0523890f6c6dee9400
Joaquin <joaquinxavier97@hotmail.com>
2024-03-17 21:18:30 -0300

    dark Mode

---
+++ script.js
@@ -0,0 +1,83 @@
+
+// Selectores
+const body = document.querySelector("body");
+const btnMode = document.querySelector("#btn-mode");
+const btnPrimary = document.querySelector(".btn");
+const title = document.querySelector("h1");
+const darkBox = document.querySelector(".darkBox");
+const showAct = document.querySelector(".actividades");
+const showActPar = document.querySelector(".actividades p");
+const cronoDark = document.querySelector(".cronograma--table");
+const cronograma = document.querySelector("#cronograma");
+const estudios = document.querySelector("#estudios");
+const estudiosTitleDark = document.querySelector(".estudios-title");
+const footer = document.querySelector(".footer");
+const showSpotify = document.querySelector("#redes-items-spotify");
+const spotify = document.querySelector(".spotify");
+
+// BOTONES MOSTRAR
+const cronoShow = document.querySelector(".cronoShow");
+const estudiosShow = document.querySelector(".estudiosShow");
+const musicosShow = document.querySelector(".musicosShow");
+
+// BOTONES OCULTAR
+const cronoHidden = document.querySelector(".cronoHidden");
+const estudiosHidden = document.querySelector(".estudiosHidden");
+
+
+
+// Dark Mode
+    btnMode.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    darkBox.classList.toggle("dark-box");
+    cronoDark.classList.toggle("cronoDark");
+    estudios.classList.toggle("estudiosDark");
+    estudiosTitleDark.classList.toggle("estudiosTitleDark");
+    footer.classList.toggle("dark-footer");
+    cronoShow.classList.toggle("btnDark");
+    estudiosShow.classList.toggle("btnDark");
+    cronoHidden.classList.toggle("btnDark");
+    estudiosHidden.classList.toggle("btnDark");
+})
+
+// TITULO DE LA PÁGINA
+
+title.innerHTML = "Permanecer 2k";
+
+// Mostrar Actividades
+showAct.addEventListener("click", () => {
+    showActPar.classList.toggle("actividadHidden");
+    showAct.classList.toggle("actividad_red");
+    showActPar.classList.toggle("actividadShow")
+})
+
+// Cronograma
+
+cronoShow.addEventListener("click", () => {
+    cronograma.style.display="flex"
+    estudios.style.display="none"
+
+})
+
+cronoHidden.addEventListener("click", () => {
+    cronograma.style.display="none";
+})
+
+// Estudios
+
+estudiosShow.addEventListener("click", () => {
+    estudios.style.display="flex"
+    cronograma.style.display="none"
+})
+
+estudiosHidden.addEventListener("click", () => {
+    estudios.style.display="none"
+})
+
+// Spotify 
+
+showSpotify.addEventListener("click", () => {
+    spotify.style.display= "block"
+    body.classList.toggle("filter-body")
+})
+
